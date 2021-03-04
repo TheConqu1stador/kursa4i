24671c347bd11c88abed37b44ed17339e855d4e8
Sakro Antes <antessial@gmail.com>
2021-03-05 01:25:27 +0300

    N п/п -> N Записи

--- Ivan Anime/04-07 - SQL Features.sql
+++ Ivan Anime/04-07 - SQL Features.sql
@@ -1,13 +1,13 @@
--- 4 - удаляет устаревшие доверенности
+-- 4 - удаляет устаревшие и неиспользуемые при этом доверенности, можно их побольше сделать
 CREATE OR REPLACE PROCEDURE public."Актуализация доверенностей"()
 	LANGUAGE plpgsql
 	AS $$
 DECLARE
 	"i" INT;
 BEGIN
-	FOR "i" IN (SELECT "№ п/п" FROM "Дов-сть авто") LOOP
-		IF ((SELECT "Дата выдачи" + make_interval(days => "Срок действия") FROM "Дов-сть авто" WHERE "№ п/п" = "i") < clock_timestamp()) THEN
-			DELETE FROM "Дов-сть авто" WHERE "№ п/п" = "i";
+	FOR "i" IN (SELECT "№ Записи" FROM "Дов-сть авто") LOOP
+		IF ((SELECT "Дата выдачи" + make_interval(days => "Срок действия") FROM "Дов-сть авто" WHERE "№ Записи" = "i") < clock_timestamp()) THEN
+			DELETE FROM "Дов-сть авто" WHERE "№ Записи" = "i";
 		END IF;
 	END LOOP;
 	FOR "i" IN (SELECT "№ Доверенности" FROM "Доверенность") LOOP
@@ -26,17 +26,24 @@ CREATE VIEW "Каталог" AS
 		   CONCAT("Шины"."Производитель", ' ', "Шины"."Модель") "Модель шин", 
 		   CONCAT("Диски"."Производитель", ' ', "Диски"."Модель") "Модель дисков",
 		   CONCAT("КПП"."Тип коробки", ', ', "КПП"."Кол-во передач", ' передач') "КПП"
-		   
+
 	FROM "Договор"
 		INNER JOIN "Авто" ON "Авто"."Гос номер" = "Договор"."Номер авто"
-		INNER JOIN "Колесо" ON "Колесо"."№ п/п" = "Авто"."№ Колёс"
-		INNER JOIN "Диски" ON "Диски"."№ п/п" = "Колесо"."№ Дисков"
-		INNER JOIN "Шины" ON "Шины"."№ п/п" = "Колесо"."№ Шин"
-		INNER JOIN "КПП" ON "Авто"."№ КПП" = "КПП"."№ п/п"
+		INNER JOIN "Колесо" ON "Колесо"."№ Записи" = "Авто"."№ Колёс"
+		INNER JOIN "Диски" ON "Диски"."№ Записи" = "Колесо"."№ Дисков"
+		INNER JOIN "Шины" ON "Шины"."№ Записи" = "Колесо"."№ Шин"
+		INNER JOIN "КПП" ON "Авто"."№ КПП" = "КПП"."№ Записи"
 
-
--- 6 - 
-
+-- 6 - А что делать то нахуй
+CREATE FUNCTION public."Металлолом"("Номер" VARCHAR(9)) RETURNS INT
+	LANGUAGE plpgsql
+	AS $$
+DECLARE
+	"Результат" INT;
+BEGIN
+	"Результат" = 0;
+END
+$$;
 
 -- 7
 
