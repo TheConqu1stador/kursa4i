e7e274ba40d15978c9caf0e3145df97b874016ae
Sakro Antes <antessial@gmail.com>
2021-03-05 00:47:54 +0300

    Update 04-07 - SQL Features.sql

--- Ivan Anime/04-07 - SQL Features.sql
+++ Ivan Anime/04-07 - SQL Features.sql
@@ -6,9 +6,16 @@ DECLARE
 	"i" INT;
 BEGIN
 	FOR "i" IN (SELECT "№ п/п" FROM "Дов-сть авто") LOOP
-			IF ((SELECT "Дата выдачи" + make_interval(days => "Срок действия") FROM "Дов-сть авто" WHERE "№ п/п" = "i") < clock_timestamp()) THEN
-				DELETE FROM "Дов-сть авто" WHERE "№ п/п" = "i";
+		IF ((SELECT "Дата выдачи" + make_interval(days => "Срок действия") FROM "Дов-сть авто" WHERE "№ п/п" = "i") < clock_timestamp()) THEN
+			DELETE FROM "Дов-сть авто" WHERE "№ п/п" = "i";
+		END IF;
+	END LOOP;
+	FOR "i" IN (SELECT "№ Доверенности" FROM "Доверенность") LOOP
+		IF (SELECT "№ Договора" FROM "Договор" WHERE "№ Доверенности" = "i") IS NULL THEN
+			IF ((SELECT "Дата выдачи" + make_interval(days => "Срок действия") FROM "Дов-сть авто" WHERE "№ Доверенности" = "i") < clock_timestamp()) THEN
+				DELETE FROM "Доверенность" WHERE "№ Доверенности" = "i";
 			END IF;
+		END IF;
 	END LOOP;
 END
 $$;
@@ -28,7 +35,7 @@ CREATE VIEW "Каталог" AS
 		INNER JOIN "КПП" ON "Авто"."№ КПП" = "КПП"."№ п/п"
 
 
--- 6
+-- 6 - 
 
 
 -- 7
