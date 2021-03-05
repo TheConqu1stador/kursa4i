fd68e401fb8feb71ea7b093cd5f1b75375ab07f7
Sakro Antes <antessial@gmail.com>
2021-03-05 16:23:55 +0300

    filled some for testing

--- Stepan Cheeeeeel/02 - filling.sql
+++ Stepan Cheeeeeel/02 - filling.sql
@@ -11,4 +11,41 @@ insert into Species (species, subspecies, marketSeedsCost, marketResultCost, wei
 	('Pea', 'Sugar snap', 0.42, 2.16, 3, '210 days'),
 	('Salad', 'Green', 0.44, 1.31, 4, '70 days');
 	
+insert into Facility (facilityName, established, staffSize) values
+	('Main', '2018-04-11', 13),
+	('Tomato node', '2019-09-12', 7),
+	('Potato node', '2020-03-27', 14),
+	('Pea node', '2021-02-03', 4);
+	
+insert into SeedsStorage (FacilityID, SpeciesID, amount, expirationDate) values
+	(1, 3, 40, '2022-03-01'),
+	(1, 4, 90, '2021-09-01'),
+	(1, 5, 240, '2021-08-01'),
+	(1, 6, 300, '2021-10-01'),
+	(1, 11, 120, '2023-05-01'),
+	(2, 1, 400, '2023-01-01'),
+	(2, 2, 500, '2022-07-01'),
+	(2, 3, 440, '2022-08-01'),
+	(3, 7, 800, '2022-09-01'),
+	(3, 8, 500, '2022-05-01'),
+	(4, 9, 600, '2025-04-01'),
+	(4, 10, 800, '2025-04-01');
+	
+insert into Bed (FacilityID, SpeciesID, soilQuality, bedSize, localBedNumber, plantedSince) values
+	(1, 3, 'Decent', 16, 1, '2020-12-21'),
+	(1, null, 'Bad', 30, 2, null),
+	(1, null, 'Decent', 30, 3, null),
+	(1, 4, 'Good', 40, 4, '2020-08-01'),
+	(1, 5, 'Good', 120, 5, '2020-09-01'),
+	(2, 1, 'Good', 100, 1, '2021-01-01'),
+	(2, 2, 'Good', 100, 2, '2021-01-01'),
+	(2, 2, 'Good', 100, 3, '2021-01-01'),
+	(2, 3, 'Decent', 100, 4, '2021-01-01'),
+	(3, 7, 'Dry', 200, 1, '2020-12-25'),
+	(3, 8, 'Dry', 200, 2, '2020-12-25'),
+	(4, 9, 'Good', 15, 1, '2021-01-03'),
+	(4, 10, 'Good', 15, 2, '2021-01-03');
+	
+
+	
 	
