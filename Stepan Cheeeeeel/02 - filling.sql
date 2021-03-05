8e393f776f9f4137c0cb700f4c3b583599492692
Sakro Antes <antessial@gmail.com>
2021-03-05 15:44:28 +0300

    updated creation, 1 table filled

---
+++ Stepan Cheeeeeel/02 - filling.sql
@@ -0,0 +1,14 @@
+insert into Species (species, subspecies, marketSeedsCost, marketResultCost, weightFromSeed, timeToGrow) values
+	('Tomato', 'Cherry', 0.02, 0.12, 40, '180 days'),
+	('Tomato', 'Grape', 0.07, 0.76, 40, '220 days'),
+	('Tomato', 'Green', 0.05, 0.43, 45, '210 days'),
+	('Artichoke', 'Globe', 0.24, 1.35, 15, '120 days'),
+	('Garlic', 'Hardneck', 0.03, 0.35, 1, '50 days'), 
+	('Garlic', 'Softneck', 0.04, 0.45, 1, '55 days'),
+	('Potato', 'Andean', 0.02, 0.34, 15, '90 days'),
+	('Potato', 'Chilean', 0.02, 0.32, 16, '90 days'),
+	('Pea', 'Snow', 0.34, 1.92, 3, '210 days'),
+	('Pea', 'Sugar snap', 0.42, 2.16, 3, '210 days'),
+	('Salad', 'Green', 0.44, 1.31, 4, '70 days');
+	
+	
