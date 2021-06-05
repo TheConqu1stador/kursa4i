20e6b5f5b4ef3bfe61b9bfa5b69fde9b7174e16b
Sakro Antes <antessial@gmail.com>
2021-06-05 14:29:30 +0300

    + 2b

--- 4l3x33v/04_Queries.sql
+++ 4l3x33v/04_Queries.sql
@@ -20,3 +20,25 @@ begin
 	where c.inStaff = true;
 end
 $$;
+
+-- 2b - view
+-- Много информации по сотрудникам
+create view View_Employees as
+	select e.Name n1, e.Age, o.Name n2, o.Address, d.Name n3, c.Salary, p.Name n4
+	from Employee e
+		inner join Department d on d.ID = e.DepartmentID
+		inner join Office o on o.ID = d.MainOfficeID
+		inner join Contract c on c.ID = e.ContractID
+		inner join Profession p on p.ID = c.ProfessionID;
+	
+	
+create function public.Query2b() returns table("Имя" text, "Возраст" integer, "Офис" text, "Адрес офиса" text, "Департамент" text, "З/п" integer, "Должность" text)
+    language plpgsql
+    as $$
+begin
+	return query
+	select *
+	from View_Employees;
+end
+$$;
+
